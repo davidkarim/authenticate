91065049009e44a9041bbf1fa58c8cfff92e3855
David Karim <davidkarim@gmail.com>
2016-05-05 16:58:40 -0400

    First commit, add users controller and page controller

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :firstname,
+  	presence: true
+  validates :lastname,
+  	presence: true
+  validates :email,
+  	presence: true,
+	uniqueness: true
+end
